1c332ed7dbdaa1294f60a607515e5f5bc20ae1a7
Antonio Vuono <antoniosvuono@icloud.com>
2024-02-17 22:09:20 -0300

    fix: database client default added

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,14 @@
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './envs'
 
-const databaseConnection =
-  env.DATABASE_CLIENT === 'sqlite'
-    ? {
-        filename: env.DATABASE_URL,
-      }
-    : env.DATABASE_URL
-
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: databaseConnection,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
